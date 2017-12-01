edfe3c5a801ff00849c6914475d8bee139dc6113
Timothy Yu <tmichael.yu@gmail.com>
2017-12-01 14:48:37 -0500

    PyPoll/main.py: variables moved out of for loop for accurate count

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,17 @@
 import csv
 import os
 
-fileNumber = ['1']
-#fileNumber = ['1', '2']
+fileNumber = ['1','2']
+
+#The following variables have to be outside of the for loop below to work
+totalVotes = 0
+candidateList = []
+uniqueList= [] 
+uniqueVoteCount = []
 
 for numToCheck in fileNumber:
 
 	electionCSV = os.path.join('raw_data', 'election_data_' + numToCheck + '.csv')
-
-	totalVotes = 0
-	candidateList = []
-	uniqueList= [] 
 
 	with open(electionCSV,'r') as csvFile:
 
@@ -21,16 +22,37 @@ for numToCheck in fileNumber:
 
 		for row in csvreader:
 			candidateList.append(row[2])
-		for row in candidateList:
-			if row not in uniqueList:
-				uniqueList.append(row)		
-				
+
+for row in candidateList:
+	if row not in uniqueList:
+		uniqueList.append(row)
+'''
+for each unique item in unique list
+	count the occurences of said unqiue item in individual counters
+	
+	then calculate percentage in relation of total votes
+
+	then calcuate winner
+''' 				
 print(uniqueList)
+
 totalVotes = len(candidateList)
-print (totalVotes)
+
+print("Election Results")
+print("----------------------------")
+print("Total Votes: " + str(totalVotes))
+print("----------------------------")
+print("----------------------------")
+print("Election winner: ")
+print("----------------------------")
+
+#------------------------------------------------------
 
 #-------------------------------------------------------
 #Debugging
+
+#load only first file (or portion of first file) to speed up debugging/testing
+#fileNumber = ['1']
 
 #for x in range (1,20):
 	#print (candidateList[x])
