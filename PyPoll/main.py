78afb131ce91cbf5ddab5a42fb5c251269d978cf
Timothy Yu <tmichael.yu@gmail.com>
2017-12-01 16:33:42 -0500

    PyPoll/main.py: results output to file implemented and output fixes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -34,31 +34,39 @@ for row in candidateList:
 		uniqueList.append(row)
 		d[row]=uniqueCount
 
-#print(d)
-
-#pseudocode for unqiue vote count per candidate 
-'''
-for each unique item in unique list
-	count the occurences of said unique item in individual counters
-	
-	then calculate percentage in relation of total votes
-
-	then calcuate winner
-''' 
-
+#max() method plus dictionary access to pull k:v pairs
 totalVotes = len(candidateList)
+winner = max(d, key=d.get)
+
+#-------------------------------------------------------
+#Terminal output of results
 
 print("Election Results")
 print("----------------------------")
 print("Total Votes: " + str(totalVotes))
 print("----------------------------")
 for candidate in d:
-	print(candidate +":" + "("+str(d[candidate]) +")")
+	percent = round(d[candidate]/totalVotes * 100,2)
+	print(candidate +": "+ str(percent) +"%" +" ("+str(d[candidate]) +")")
 print("----------------------------")
-print("Election winner: ")
+print("Election winner: " + winner)
 print("----------------------------")
 
-#------------------------------------------------------
+#-------------------------------------------------------
+#Save results to file
+
+with open('output.txt', 'w+') as file:
+	file.write("Election Results\n")
+	file.write("----------------------------\n")
+	file.write("Total Votes: " + str(totalVotes)+"\n")
+	file.write("----------------------------\n")
+	for candidate in d:
+		percent = round(d[candidate]/totalVotes * 100,2)
+		file.write(candidate +": "+ str(percent) +"%" +" ("+str(d[candidate]) +")\n")
+	file.write("----------------------------\n")
+	file.write("Election winner: " + winner+"\n")
+	file.write("----------------------------\n")
+	file.close()
 
 #-------------------------------------------------------
 #Debugging
@@ -71,5 +79,15 @@ print("----------------------------")
 
 #print(uniqueList)
 
+
+#pseudocode for unqiue vote count per candidate 
+'''
+for each unique item in unique list
+	count the occurences of said unique item in individual counters
+	
+	then calculate percentage in relation of total votes
+
+	then calcuate winner
+''' 
 	
 
