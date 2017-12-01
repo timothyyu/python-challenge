a4085fb2d4bb4cee292704f8d7e7f0f21c8cdda4
Timothy Yu <tmichael.yu@gmail.com>
2017-12-01 12:38:20 -0500

    PyBank/main.py: minor output fixes an pseudocode for period definition added

--- PyBank/main.py
+++ PyBank/main.py
@@ -33,7 +33,8 @@ for numToCheck in fileNumber:
 
         for row in csvreader:
             date.append(row[0])
-            revenue.append(row[1])
+            revenue.append(row[1])  
+            #revenue.append(int(row[1])) results in type error for int + str 
             totalRevenue = totalRevenue + int(row[1])
 
             if row != 1 & referenceChangeFlag == False:
@@ -51,11 +52,27 @@ for numToCheck in fileNumber:
                     greatestIncreaseRevenue = runningChange
                     greatestIncreaseRevenueDate = row[0]
 
+    #Zip function to Save the combined CSV data to a new file; not required
     #combinedBudgetCSV = zip(date, revenue)
 
+#period definition for output, i.e. what is the definition of one period for the analysis? 
+'''
+userinput [y/n] to use 12 months as period definition
+output:
+    Total months 
+    Period count 
+        periodlen(date)/12 (rounded?)
+    average revenue change (per period, averaged together)
+    greatest increase in revenue, month/year, period count, $ increase
+    greatest increase in revenue, month/year, period count, $ decrease
+
+'''
 monthTotal=len(date)
+print("Using total number of months as period for analysis...")
 averageRevenue = round(sum(averageRevenueChange)/monthTotal)
 
+
+pseudo 
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(monthTotal))
@@ -69,18 +86,19 @@ print("----------------------------")
 #Write terminal results to new file
 
 with open('output.txt', 'w+') as file:
-    #print >> f, 'Filename:', filename  # Python 2.x
-    file.write("Financial Analysis" + '\n')
-    file.write("----------------------------" + '\n')
+    #Triple quotes to enclose multiple lines in print statement is possible; specifics for variables needed to use correctly
+    file.write("Financial Analysis\n")
+    file.write("----------------------------\n")
     file.write("Total Months: " + str(monthTotal)+ '\n')
     file.write("Total Revenue: $" + str(totalRevenue)+ '\n')
     file.write("Average Revenue Change: $" + str(averageRevenue)+ '\n')
-    file.write("Greatest Increase in Revenue: " + str(greatestIncreaseRevenueDate) + " ($"+str(greatestIncreaseRevenue)+")"+ '\n')
-    file.write("Greatest Decrease in Revenue: " + str(greatestDecreaseRevenueDate) + " ($"+str(greatestDecreaseRevenue)+")"+ '\n')
-    file.write("----------------------------"+ '\n')
+    file.write("Greatest Increase in Revenue: " + str(greatestIncreaseRevenueDate) + " ($"+str(greatestIncreaseRevenue)+")\n")
+    file.write("Greatest Decrease in Revenue: " + str(greatestDecreaseRevenueDate) + " ($"+str(greatestDecreaseRevenue)+")\n")
+    file.write("----------------------------\n")
     file.close()
+
 #-------------------------------------------------------
-#Save the combined CSV data to a new file
+#Save the combined CSV data to a new file - not required 
 
 #with open(newBudgetCSV, 'w', newline='') as csvFile:
 
